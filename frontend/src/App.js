d1e91639c44602c3f763555650450e231037a3c9
Lim Bing Sen <bingsenlim0806@gmail.com>
2021-06-10 09:56:15 +0800

    Modify App.js a bit after merging

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,7 +4,7 @@ import Register from "./pages/register/Register";
 import UserListing from "./pages/userlisting/UserListing";
 import Review from "./pages/review/Review";
 import Product from "./pages/product/Product";
-import CreateLising from "./pages/createListing/CreateListing";
+import CreateListing from "./pages/createListing/CreateListing";
 import Item from "./pages/item/Item";
 import {
   BrowserRouter as Router,
@@ -21,23 +21,22 @@ function App() {
   const { user } = useContext(AuthContext);
 
   return (
-    // <Router>
-    //   <Switch>
-    //     <Route exact path="/">
-    //       {user ? <Home /> : <Register />}
-    //     </Route>
-    //     <Route path="/login">
-    //       {user ? <Redirect to="/chat" /> : <Login />}
-    //     </Route>
-    //     <Route path="/register">
-    //       {user ? <Redirect to="/" /> : <Register />}
-    //     </Route>
-    //     <Route exact path="/chat">
-    //       {user ? <Chat /> : <Register />}
-    //     </Route>
-    //   </Switch>
-    // </Router>
-    <Item />
+    <Router>
+      <Switch>
+        <Route exact path="/">
+          {user ? <Home /> : <Register />}
+        </Route>
+        <Route path="/login">
+          {user ? <Redirect to="/chat" /> : <Login />}
+        </Route>
+        <Route path="/register">
+          {user ? <Redirect to="/" /> : <Register />}
+        </Route>
+        <Route exact path="/chat">
+          {user ? <Chat /> : <Register />}
+        </Route>
+      </Switch>
+    </Router>
   );
 }
 
