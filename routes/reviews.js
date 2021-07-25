c5a645c5c0cc782827af0ae3e9d7013f06a04a91
Lim Bing Sen <bingsenlim0806@gmail.com>
2021-07-26 01:51:16 +0800

    Sort review api based on createdAt

--- routes/reviews.js
+++ routes/reviews.js
@@ -18,7 +18,7 @@ router.get("/profileUser/:id", async (req, res) => {
   try {
     const reviews = await Review.find({
       revieweeId: req.params.id,
-    });
+    }).sort({ createdAt: -1 });
     res.status(200).json(reviews);
   } catch (err) {
     res.status(500).json(err);
